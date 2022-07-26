fe806d7c92e8c8cf8b1937b2e80777c04915915a
gstrdms <86445295+gstrdms@users.noreply.github.com>
2022-07-26 18:49:35 +0000

    Adding an 'end of message' delimiter

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import base64
  
+ENDOFMESSAGE = "0100100101010101010101100100111101010010010001010011100101000111010101000101010101010110010101000101010100110000010001100100100001010010010100110100010100111101"
+ 
 def encode_message_as_bytestring(message):
     b64 = message.encode("utf8")
     bytes_ = base64.encodebytes(b64)
     bytestring = "".join(["{:08b}".format(x) for x in bytes_])
+    bytestring += ENDOFMESSAGE
     return bytestring
