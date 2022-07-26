e6d77f988f9b5d4ed8298f006a627c3e47d60826
gstrdms <86445295+gstrdms@users.noreply.github.com>
2022-07-26 18:58:16 +0000

    Getting pixels from an image

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import base64
+
+import png
  
 ENDOFMESSAGE = "0100100101010101010101100100111101010010010001010011100101000111010101000101010101010110010101000101010100110000010001100100100001010010010100110100010100111101"
  
@@ -8,3 +10,8 @@ def encode_message_as_bytestring(message):
     bytestring = "".join(["{:08b}".format(x) for x in bytes_])
     bytestring += ENDOFMESSAGE
     return bytestring
+
+def get_pixels_from_image(fname):
+    img = png.Reader(fname).read()
+    pixels = img[2]
+    return pixels
